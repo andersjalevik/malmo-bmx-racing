c40f329f2cb97ce1d5c6c08ac27ce29ad0644c14
malmobmxracing <malmobmxracing@gmail.com>
2026-01-17 21:10:12 +0100

    test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,6 +16,12 @@ const Footer = class extends React.Component {
         <a href='https://malmo.se/' target='_blank'><Logo src="/img/logo_malmo.png" /></a>
         <a href='https://storbildsbolaget.se/' target='_blank'><Logo src="/img/logo_sbb.png" /></a>
         <a href='https://www.miljofabriken.com/' target='_blank'><Logo src="/img/miljofabriken.png" /></a>
+        <a target='_blank'><Logo src="/static/img/Åkerblom & Hansson Ekonomi & Skattekonsult AB Horisontell.jpg" /></a>
+
+
+
+
+
         </Row>
       </StyledFooter>
     )
