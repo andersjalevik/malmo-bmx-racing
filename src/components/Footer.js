5719a11e7d58e7e2310c9bae954ae3082a793e47
malmobmxracing <malmobmxracing@gmail.com>
2026-01-17 21:11:55 +0100

    test2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ const Footer = class extends React.Component {
         <a href='https://www.lyft-byggmaskiner.se/kontakt/malmo/' target='_blank'><Logo src="/img/logo_lbm.png" /></a>
         <a href='https://malmo.se/' target='_blank'><Logo src="/img/logo_malmo.png" /></a>
         <a href='https://storbildsbolaget.se/' target='_blank'><Logo src="/img/logo_sbb.png" /></a>
-        <a href='https://www.miljofabriken.com/' target='_blank'><Logo src="/img/miljofabriken.png" /></a>
+        <a href='' target='_blank'><Logo src="/img/miljofabriken.png" /></a>
         <a target='_blank'><Logo src="/static/img/Åkerblom & Hansson Ekonomi & Skattekonsult AB Horisontell.jpg" /></a>
 
 
