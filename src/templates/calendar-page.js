f14c5ef444d529cd339ffd1633bae58ea6555ad3
andersjalevik <anders.jalevik@gmail.com>
2026-01-29 20:01:30 +0100

    Update calendar-page.js
    Minor change just to test new netlify build image triggering correctly

--- src/templates/calendar-page.js
+++ src/templates/calendar-page.js
@@ -12,7 +12,7 @@ export const IndexPageTemplate = ({
     <h1>{title}</h1>
     <div dangerouslySetInnerHTML={{ __html: html }} />
     <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Europe%2FStockholm&mode=AGENDA&showTitle=0&showDate=0&showPrint=0&showTabs=0&showCalendars=1&showTz=0&showNav=0&title&src=Zmdpbm5nYmhjbXA2aHRiNHE3a3FjbTE0Z2NAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23E67C73"
-            style={{borderWidth:0}} width="100%" height="600" frameborder="0" scrolling="no"></iframe>
+            style={{borderWidth:0}} width="100%" height="599" frameborder="0" scrolling="no"></iframe>
     </div>
 
 )}
