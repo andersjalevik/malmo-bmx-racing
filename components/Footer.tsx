6f1325e032cd1159ea3e5b3d91cd629161a78929
malmobmxracing <malmobmxracing@gmail.com>
2026-05-10 17:51:31 +0200

    3 nya sponsorer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -102,6 +102,15 @@ export default function Footer() {
         <a href="https://jonab.se" target="_blank" rel="noopener noreferrer">
           <Logo src="/img/fea833c0-050d-4d23-a967-8081d6afb848.jpg" alt="Sponsor" />
         </a>
+          <a href="/" target="_blank" rel="noopener noreferrer">
+          <Logo src="/img/Aderian_Navet_blue_rgb.png" alt="Sponsor" />
+        </a>
+          <a href="/" target="_blank" rel="noopener noreferrer">
+          <Logo src="/img/Trädtjänst LOGO hemsida RGB.png" alt="Sponsor" />
+        </a>
+          <a href="/" target="_blank" rel="noopener noreferrer">
+          <Logo src="/img/1778428149087-b5ac05ca-ad87-4fae-8c49-8a0151c61f7c_1.png" alt="Sponsor" />
+        </a>
       </Row>
     </StyledFooter>
   )
